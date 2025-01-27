d23a0ad815c33b1b464aa72c3ed908efcb701f4e
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2025-01-26 18:17:47 -0600

    add theme toggle to ui

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { createGame, ping } from "./actions";
 import GamesGrid from "@/components/ui/games-grid";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <ThemeToggle />
         <Button onClick={ping}>Ping</Button>
         <Button onClick={createGame}>Create Game</Button>
         <GamesGrid />
@@ -13,3 +15,4 @@ export default function Home() {
     </div>
   );
 }
+
