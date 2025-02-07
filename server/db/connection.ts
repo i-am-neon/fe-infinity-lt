2c84b272cb1cc6f2a75a57b7a617b1adeb6c5e36
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2025-02-07 10:45:37 -0600

    use absolute path in db connection

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -1,9 +1,10 @@
 import { DB } from "https://deno.land/x/sqlite/mod.ts";
+import { getPathWithinServer } from "@/file-io/get-path-within-server.ts";
 
 /**
  * Create (or open) a local SQLite database file named "my_local.db"
  * located in the server/db/ folder.
  */
-const dbPath = "./db/local.db";
+const dbPath = getPathWithinServer("/db/local.db");
 export const db = new DB(dbPath);
 db.execute("PRAGMA journal_mode = WAL;");
