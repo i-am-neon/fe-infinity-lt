86141cb7b0ac76e0641a7450f5731bded45c1d4f
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2025-02-08 16:00:05 -0600

    add disallowedMapNames

--- server/map-processing/lib/get-all-map-json-files.ts
+++ server/map-processing/lib/get-all-map-json-files.ts
@@ -1,10 +1,15 @@
 import { getPathWithinServer } from "@/file-io/get-path-within-server.ts";
+import { disallowedMapNames } from "@/map-processing/lookup-tables/disallowed-map-names.ts";
 
 export default function getAllMapNames() {
   const mapNames: string[] = [];
   for (const entry of Deno.readDirSync(getPathWithinServer("assets/maps"))) {
     if (entry.isFile && entry.name.endsWith(".json")) {
-      mapNames.push(entry.name.slice(0, -5));
+      const mapName = entry.name.slice(0, -5);
+      if (disallowedMapNames.includes(mapName)) {
+        continue;
+      }
+      mapNames.push(mapName);
     }
   }
   return mapNames;
---
+++ server/map-processing/lookup-tables/disallowed-map-names.ts
@@ -0,0 +1,8 @@
+export const disallowedMapNames = [
+  "(7)Ch29x31xBattlePreparations_ArenaTime__by_Shin19",
+  "Xenithiagen_FE8_1800481A_Thracia776Chapter24__by_FEU",
+  "Ukulele_FE8_8800898A_Blitz__by_FEU",
+  "Doors_to_Destiny_Part_2__by_ZoramineFae",
+  "Chapter13RescueMission_Diff_Tileset__by_Shin19",
+];
+
