76bffa9c87b6f75c8527b066b6babf50871fe800
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2025-02-21 19:44:48 -0600

    Update affinity.ts

--- server/types/character/affinity.ts
+++ server/types/character/affinity.ts
@@ -1,13 +1,10 @@
 import { z } from "zod";
 
-export const AffinitySchema = z.enum([
-  "Anima",
-  "Dark",
-  "Fire",
-  "Ice",
-  "Light",
-  "Thunder",
-  "Wind",
-]);
+export const AffinitySchema = z
+  .enum(["Anima", "Dark", "Fire", "Ice", "Light", "Thunder", "Wind"])
+  .describe(
+    'Must be one of these and not anything else: "Anima", "Dark", "Fire", "Ice", "Light", "Thunder", "Wind"'
+  );
 
 export type Affinity = z.infer<typeof AffinitySchema>;
+
