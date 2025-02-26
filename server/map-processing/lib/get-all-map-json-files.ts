c5ceb1cc13d8ddcee205a00e825db4f5fa655c41
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2025-02-26 16:31:25 -0600

    rename

--- server/map-processing/lib/get-all-map-json-files.ts
+++ server/map-processing/lib/get-all-map-json-files.ts
@@ -1,12 +1,12 @@
 import { getPathWithinServer } from "@/file-io/get-path-within-server.ts";
-import { disallowedMapNames } from "@/map-processing/lookup-tables/disallowed-map-names.ts";
+import { ignoredMapNames } from "../lookup-tables/ignored-map-names.ts";
 
 export default function getAllMapNames() {
   const mapNames: string[] = [];
   for (const entry of Deno.readDirSync(getPathWithinServer("assets/maps"))) {
     if (entry.isFile && entry.name.endsWith(".json")) {
       const mapName = entry.name.slice(0, -5);
-      if (disallowedMapNames.includes(mapName)) {
+      if (ignoredMapNames.includes(mapName)) {
         continue;
       }
       mapNames.push(mapName);
--- server/map-processing/lookup-tables/ignored-map-names.ts
+++ server/map-processing/lookup-tables/ignored-map-names.ts
@@ -1,9 +1,10 @@
-export const disallowedMapNames = [
+export const ignoredMapNames = [
   "(7)Ch29x31xBattlePreparations_ArenaTime__by_Shin19",
   "Xenithiagen_FE8_1800481A_Thracia776Chapter24__by_FEU",
   "Ukulele_FE8_8800898A_Blitz__by_FEU",
   "Doors_to_Destiny_Part_2__by_ZoramineFae",
   "Chapter13RescueMission_Diff_Tileset__by_Shin19",
   "Nobles_Evil_Doers_10_(88_00_89_8A)__by_Aura_Wolf",
+  "Blyvern_FE8_3C003D3E_dlitzbicks__by_FEU",
 ];
 
