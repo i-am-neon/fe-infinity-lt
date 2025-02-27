800cb0906389c24ce683a2014e066794e3af4fa3
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2025-02-27 14:06:06 -0600

    add breakable wall sprites

---
+++ server/map-region-processing/breakable-wall-sprites.ts
@@ -0,0 +1,50 @@
+// Record of tileset id and coordinates for open chests
+export const breakableWallSprites: Record<string, { x: number; y: number }[]> =
+  {
+    // "03": [{ x: 1, y: 0 }], // no breakable walls on this map
+    "10": [
+      { x: 18, y: 23 },
+      { x: 17, y: 23 },
+    ],
+    "1A": [
+      { x: 17, y: 15 },
+      { x: 16, y: 15 },
+    ],
+    "30": [
+      { x: 11, y: 25 },
+      { x: 10, y: 25 },
+    ],
+    "3E": [
+      { x: 12, y: 16 },
+      { x: 12, y: 17 },
+      { x: 13, y: 17 },
+      { x: 14, y: 17 },
+      { x: 14, y: 18 },
+    ],
+    // "44": [{ x: 0, y: 0 }], // No breakable walls on this map
+    "52": [
+      { x: 15, y: 14 },
+      { x: 2, y: 17 },
+      { x: 3, y: 17 },
+    ],
+    "61": [
+      { x: 7, y: 28 },
+      { x: 8, y: 28 },
+      { x: 9, y: 28 },
+    ],
+    "6E": [
+      { x: 6, y: 29 },
+      { x: 7, y: 28 },
+      { x: 8, y: 28 },
+    ],
+    // "7B": [{ x: 13, y: 0 }], // No breakable walls on this map
+    "8A": [
+      { x: 18, y: 12 },
+      { x: 19, y: 12 },
+      { x: 23, y: 15 },
+      { x: 24, y: 15 },
+      { x: 23, y: 16 },
+      { x: 24, y: 16 },
+    ],
+  };
+
