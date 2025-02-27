7bbf7f54865e06cdda01a136aae7490d73a5a0fa
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2025-02-27 11:15:36 -0600

    add door sprites

---
+++ server/map-region-processing/door-sprites.ts
@@ -0,0 +1,61 @@
+// Record of tileset id and coordinates for open chests
+export const doorSprites: Record<string, { x: number; y: number }[]> = {
+  // "03": [{ x: 1, y: 0 }], // No doors on this map
+  "10": [
+    { x: 4, y: 11 },
+    { x: 5, y: 1 },
+    { x: 10, y: 19 },
+    { x: 11, y: 19 },
+  ],
+  "1A": [
+    { x: 0, y: 10 },
+    { x: 0, y: 13 },
+    { x: 0, y: 13 },
+    { x: 1, y: 13 },
+    { x: 2, y: 13 },
+    { x: 0, y: 14 },
+    { x: 0, y: 15 },
+    { x: 7, y: 14 }, // vertical door
+  ],
+  "30": [
+    { x: 7, y: 24 },
+    { x: 8, y: 24 },
+    { x: 10, y: 27 },
+    { x: 11, y: 27 },
+  ],
+  "3E": [
+    { x: 0, y: 13 },
+    { x: 1, y: 13 },
+    { x: 2, y: 13 },
+    { x: 0, y: 14 },
+    { x: 0, y: 15 },
+    { x: 0, y: 16 },
+    { x: 3, y: 23 },
+    { x: 3, y: 24 },
+  ],
+  // "44": [{ x: 0, y: 0 }], // No doors on this map
+  "52": [
+    { x: 0, y: 14 },
+    { x: 1, y: 14 },
+    { x: 2, y: 14 },
+  ],
+  // "61": [{ x: 1, y: 0 }], // No doors on this map
+  "6E": [
+    { x: 14, y: 27 },
+    { x: 15, y: 27 },
+    { x: 25, y: 28 },
+  ],
+  "7B": [
+    { x: 13, y: 0 },
+    { x: 14, y: 0 },
+    { x: 15, y: 0 },
+    { x: 13, y: 1 },
+    { x: 14, y: 1 },
+    { x: 15, y: 1 },
+  ],
+  "8A": [
+    { x: 3, y: 0 },
+    { x: 8, y: 2 },
+  ],
+};
+
