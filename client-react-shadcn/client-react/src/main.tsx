0e2691fdfb54632194844cadd704d2e1cc89f468
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2025-03-07 14:44:46 -0600

    create new dir for working shadcn

---
+++ client-react-shadcn/client-react/src/main.tsx
@@ -0,0 +1,32 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import "./index.css";
+
+import GameDetailPage from "./routes/game-detail-page";
+import HomePage from "./routes/home-page";
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "games/:nid",
+        element: <GameDetailPage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
